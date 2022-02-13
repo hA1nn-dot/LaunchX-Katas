0cd15dfc212d894d4842ac871873c30ac32d19e9
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-02-12 22:20:30 -0600

    Create: kata 3

---
+++ Kata 3/kata3.py
@@ -0,0 +1,27 @@
+asteroide = 49
+if asteroide > 25:
+    print('¡Alerta! ¡Un asteroide se acerca a velocidades peligrosas!')
+else:
+    print('¡Sigue con tu día!')
+
+
+
+asteroide = 19
+if asteroide > 20:
+    print('Look up! ¡Hay una luz mágica en el cielo!')
+elif asteroide == 20:
+    print('Look up! ¡Hay una luz mágica en el cielo!')
+else:
+    print('¡Nada que ver aquí!')
+
+
+velocidad_asteroide = 25
+tamano_asteroide = 40
+if velocidad_asteroide > 25 and tamano_asteroide > 25:
+    print('¡Alerta, Un asteroide muy peligroso viene hacia la Tierra!')
+elif velocidad_asteroide >= 20:
+    print('Look up! ¡Hay una luz mágica en el cielo!')
+elif tamano_asteroide < 25:
+    print('Nada que ver aquí :)')
+else:
+    print('Nada que ver aquí :)')

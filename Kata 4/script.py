21cd9b5d73fc5234c32d6fd964e729e8c3a5b2bb
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-02-12 22:30:56 -0600

    Create Files kata 4

---
+++ Kata 4/script.py
@@ -0,0 +1,15 @@
+text = """Interesting facts about the Moon. The Moon is Earth's only satellite. There are several interesting facts about the Moon and how it affects life here on Earth. 
+On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth. The highest daylight temperature of the Moon is 127 C."""
+
+#Primero, divide el texto en cada oración para trabajar con su contenido:
+oracion1 = "Interesting facts about the Moon. The Moon is Earth's only satellite. There are several interesting facts about the Moon and how it affects life here on Earth."
+oracion2 = "On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth."
+oracion3 = "The highest daylight temperature of the Moon is 127 C."
+
+
+#Ahora, define algunas palabras clave para búsqueda que te ayudarán a determinar si una oración contiene un hecho.
+## Define las palabras pista: average, temperature y distance suenan bien
+average = "average"
+temperature = "temperature"
+distance = "distance"
+

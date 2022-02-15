dacab9be7d30f8d875f9206b1563ec245e2e03ba
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-02-15 12:44:49 -0600

    Complete: kata4

--- Kata 4/script.py
+++ Kata 4/script.py
@@ -3,7 +3,7 @@ On average, the Moon moves 4cm away from the Earth every year. This yearly drift
 
 #Primero, divide el texto en cada oración para trabajar con su contenido:
 text_parts = text.split('. ')
-text_parts
+#text_parts
 
 #Ahora, define algunas palabras clave para búsqueda que te ayudarán a determinar si una oración contiene un hecho.
 ## Define las palabras pista: average, temperature y distance suenan bien
@@ -12,12 +12,51 @@ key_words = ["average", "temperature", "distance"]
 for sentence in text_parts:
     for key_word in key_words:
         if key_word in sentence:
-            print(sentence)
+            #print(sentence)
             break
 
 # Ciclo para cambiar C a Celsius
 for sentence in text_parts:
     for key_word in key_words:
         if key_word in sentence:
-            print(sentence.replace(' C', ' Celsius'))
+            #print(sentence.replace(' C', ' Celsius'))
             break
+
+#Ejercicio 2
+
+planeta = 'Marte '
+gravedad  = 0.00143
+nombre = 'Ganímedes'
+title = f'datos de gravedad sobre {nombre}'
+
+
+# Creamos la plantilla
+hechos = f"""{'-'*80} 
+Nombre del planeta: {planeta} 
+Gravedad en {nombre}: {gravedad * 1000} m/s2 
+"""
+
+#print(hechos)
+
+template = f"""{title.title()} 
+{hechos} 
+""" 
+
+
+# Nuevos datos muestra
+planeta = 'Marte '
+gravedad  = 0.00143
+nombre = 'Ganímedes'
+
+#print(hechos)
+
+new_template = """
+Datos de Gravedad sobre: {nombre}
+-------------------------------------------------------------------------------
+Nombre del planeta: {planeta}
+Gravedad en {nombre}: {gravedad} m/s2
+"""
+#print(new_template.format(nombre=nombre, planeta=planeta, gravedad=gravedad))
+
+# Pista: print(nueva_plantilla.format(variables))
+print(new_template.format(nombre=nombre, planeta=planeta, gravedad=gravedad*1000))
